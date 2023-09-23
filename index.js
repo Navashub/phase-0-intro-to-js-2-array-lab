6f0417ee10b86804d687d6903214ba8afe835847
Navashub <navasmuller01@gmail.com>
2023-09-23 12:18:03 +0300

    test checked

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+      cats.length = 0;
+
+      cats.push('Milo', 'Otis', 'Garfield');
+    });
+
+function destructivelyAppendCat(Ralph){
+    cats.push("Ralph");
+
+}
+
+
+function destructivelyPrependCat(Bob){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop("Garfield");
+}
+
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift("Milo");
+}
+
+function appendCat(Broom){
+    const newCats = [...cats, Broom];
+    return newCats;
+}
+
+function prependCat(Arnold){
+    const newCats = [Arnold, ...cats];
+    return newCats;
+}
+
+function removeLastCat(){
+    const newCats = cats.slice(0, -1);
+    return newCats;
+}
+
+function removeFirstCat(){
+    const newCats = cats.slice(1);
+    return newCats;
+}
